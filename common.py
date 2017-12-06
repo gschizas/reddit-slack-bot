28e874465aa22fd93191834d80525fddc50ee54a
George Schizas <gschizas@gmail.com>
2017-12-06 10:31:09 +0200

    Use new YAML api (>=0.15)

--- common.py
+++ common.py
@@ -1,12 +1,15 @@
-from ruamel import yaml
+from pathlib import Path
+
+from ruamel.yaml import YAML
+
+_yaml = YAML(typ='safe')
+_yaml.default_flow_style = False
+_out = Path('config') / 'flair-bot.yml'
 
 
 def write_config(cfg):
-    with open('config/flair-bot.yml', encoding='utf8', mode='w') as fp:
-        yaml.safe_dump(cfg, fp, default_flow_style=False)
+    _yaml.dump(cfg, _out)
 
 
 def read_config():
-    with open('config/flair-bot.yml', encoding='utf8', mode='r') as fp:
-        cfg = yaml.safe_load(fp)
-    return cfg
+    return _yaml.load(_out)
