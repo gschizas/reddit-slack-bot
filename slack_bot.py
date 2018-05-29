3faa3f8679a0706e830ace09e3416530e9f4ae22
George Schizas <gschizas@gmail.com>
2018-05-29 20:25:06 +0300

    Switch to using cmd for command parsing

--- slack_bot.py
+++ slack_bot.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import base64
+import cmd
 import datetime
+import io
 import json
 import logging
 import logging.handlers
@@ -64,6 +66,9 @@ def main():
         sys.exit(1)
 
     teams = {}
+    shell = SlackbotShell()
+    if subreddit_name:
+        shell.sr = r.subreddit(subreddit_name)
 
     while True:
         for msg in sc.rtm_read():
@@ -87,17 +92,21 @@ def main():
             text = msg['text']
 
             if text.lower().startswith(trigger_word):
-                if subreddit_name:
-                    sr = r.subreddit(subreddit_name)
-                else:
-                    sr = None
-                reply_text, extra_data = process_command(sr, text)
-                if reply_text:
-                    if extra_data and 'image' in extra_data:
-                        result = sc.api_call("files.upload", channels=channel_id, file=extra_data['image'])
-                        logger.info(result)
-                    else:
-                        sc.api_call("chat.postMessage", channel=channel_id, text=reply_text, icon_emoji=':robot_face:', username=trigger_word)
+                line = ' '.join(text.lower().split()[1:])
+                shell.channel_id = channel_id
+                shell.team_id = team_id
+                line = shell.precmd(line)
+                stop = shell.onecmd(line)
+                stop = shell.postcmd(stop, line)
+                if stop:
+                    sys.exit()
+                # reply_text, extra_data = process_command(sr, text)
+                # if reply_text:
+                #     if extra_data and 'image' in extra_data:
+                #         
+                #         logger.info(result)
+                #     else:
+                #         
         time.sleep(1)
 
 
@@ -134,117 +143,164 @@ def process_command(sr, text):
         logger.info(args)
         return None, None
 
+class SlackbotShell(cmd.Cmd):
+    def __init__(self, **kwargs):
+        super().__init__(self, stdout=io.StringIO(), **kwargs)
+        self.sr = None
+        self.pos = 0
 
-def do_crypto_price(args):
-    """Display the current exchange rate of currency"""
-    cryptocoin = args[1].upper()
-    prices = requests.get("https://min-api.cryptocompare.com/data/price",
-                          params={'fsym': cryptocoin, 'tsyms': 'USD,EUR'}).json()
-    if prices.get('Response') == 'Error':
-        text = prices['Message']
-    else:
-        text = f"{cryptocoin} price is € {prices['EUR']} or $ {prices['USD']}"
-    return text
+    def _send_text(self, text):
+        sc.api_call("chat.postMessage", 
+            channel=self.channel_id,
+            text=text,
+            icon_emoji=':robot_face:',
+            username=trigger_word)
 
+    def _send_image(self, file_data):
+        sc.api_call("files.upload",
+            channels=self.channel_id,
+            file=file_data)
 
-def do_weather(place):
-    """Display the weather in place"""
-    if place.lower() == 'macedonia' or place.lower() == 'makedonia':
-        place = 'Thessaloniki'
-    weather = requests.get('http://wttr.in/' + place + '_p0.png')
-    return weather.ok, {'image': weather.content}
-
-
-def do_do_conversion(value_text, currency_from, currency_to):
-    """Convert money from one currency to another"""
-    try:
-        value = float(value_text)
-    except ValueError:
-        return f"{value_text} is not a good number"
-
-    if not(re.match('^\w+$', currency_from)):
-        return f"{currency_from} is not a real currency"
-
-    if not(re.match('^\w+$', currency_to)):
-        return f"{currency_to} is not a real currency"
-
-    currency_from = currency_from.upper()
-    currency_to = currency_to.upper()
-
-    if currency_from == currency_to:
-        return "Tautological bot is tautological"
-
-    prices_page = requests.get("https://min-api.cryptocompare.com/data/price",
-                          params={'fsym': currency_from, 'tsyms': currency_to})
-    logging.info(prices_page.url)
-    prices = prices_page.json()
-    if prices.get('Response') == 'Error':
-        text = prices['Message']
-    else:
-        price = prices[currency_to]
-        new_value = value * price
-        text = f"{value:.2f} {currency_from} is {new_value:.2f} {currency_to}"
-    return text
+    def postcmd(self, stop, line):
+        self.stdout.flush()
+        self.stdout.seek(self.pos, io.SEEK_SET)
+        text = self.stdout.read()
+        self.stdout.close()
+        self.stdout = io.StringIO()
+        # self.pos = self.stdout.seek(0, io.SEEK_CUR)
+        if text != '':
+            self._send_text('```\n' + text.strip() + '```\n')
+        return stop
 
 
-def do_usernotes(sr, args):
-    """Display usernotes of a user"""
-    redditor_username = args[1]
-    tb_notes = sr.wiki['usernotes']
-    tb_notes_1 = json.loads(tb_notes.content_md)
-    warnings = tb_notes_1['constants']['warnings']
-    tb_notes_2 = json.loads(zlib.decompress(base64.b64decode(tb_notes_1['blob'])).decode())
-    redditor = r.redditor(redditor_username)
-    try:
-        redditor._fetch()
-        redditor_username = redditor.name  # fix capitalization
-        notes = tb_notes_2.get(redditor_username)
+    def do_crypto_price(self, arg):
+        """Display the current exchange rate of currency"""
+        args = arg.split()
+        cryptocoin = args[1].upper()
+        prices = requests.get("https://min-api.cryptocompare.com/data/price",
+                              params={'fsym': cryptocoin, 'tsyms': 'USD,EUR'}).json()
+        if prices.get('Response') == 'Error':
+            text = prices['Message']
+        else:
+            text = f"{cryptocoin} price is € {prices['EUR']} or $ {prices['USD']}"
+        self._send_text(text)
+
+
+    def do_weather(self, arg):
+        """Display the weather in place"""
+        place = arg.lower()
+        if place == 'macedonia' or place == 'makedonia':
+            place = 'Thessaloniki'
+        weather = requests.get('http://wttr.in/' + place + '_p0.png')
+        self._send_image(weather.content)
+
+
+    def do_do_conversion(self, arg):
+        """Convert money from one currency to another"""
+        args = arg.split()
+        if len(args) != 3:
+            self._send_text(f"Argument count error.")
+            return
+
+        value_text, currency_from, currency_to = arg_parts
+
+        try:
+            value = float(value_text)
+        except ValueError:
+            self._send_text(f"{value_text} is not a good number")
+            return
+
+        if not(re.match(r'^\w+$', currency_from)):
+            self._send_text(f"{currency_from} is not a real currency")
+            return
+
+        if not(re.match(r'^\w+$', currency_to)):
+            self._send_text(f"{currency_to} is not a real currency")
+            return
+
+        currency_from = currency_from.upper()
+        currency_to = currency_to.upper()
+
+        if currency_from == currency_to:
+            self._send_text("Tautological bot is tautological")
+            return
+
+        prices_page = requests.get("https://min-api.cryptocompare.com/data/price",
+                              params={'fsym': currency_from, 'tsyms': currency_to})
+        logging.info(prices_page.url)
+        prices = prices_page.json()
+        if prices.get('Response') == 'Error':
+            text = prices['Message']
+        else:
+            price = prices[currency_to]
+            new_value = value * price
+            text = f"{value:.2f} {currency_from} is {new_value:.2f} {currency_to}"
+        self._send_text(text)
+
+
+    def do_usernotes(self, arg):
+        """Display usernotes of a user"""
+        args = arg.split()
+        redditor_username = args[1]
+        tb_notes = self.sr.wiki['usernotes']
+        tb_notes_1 = json.loads(tb_notes.content_md)
+        warnings = tb_notes_1['constants']['warnings']
+        tb_notes_2 = json.loads(zlib.decompress(base64.b64decode(tb_notes_1['blob'])).decode())
+        redditor = r.redditor(redditor_username)
+        try:
+            redditor._fetch()
+            redditor_username = redditor.name  # fix capitalization
+            notes = tb_notes_2.get(redditor_username)
+            text = ''
+            if notes is None:
+                self._send_text(f"user {redditor_username} doesn't have any user notes")
+                return
+
+            for note in notes['ns']:
+                warning = warnings[note['w']]
+                when = datetime.datetime.fromtimestamp(note['t'])
+                note = note['n']
+                text += (f"<!date^{int(when.timestamp())}^{warning} at {{date_short}} {{time}}: {note}|"
+                         f"{warning} at {when.isoformat()}: {note}>\n")
+            self._send_text(text)
+            return
+        except prawcore.exceptions.NotFound:
+            self._send_text(f"user {redditor_username} not found")
+            return
+
+
+    def do_modqueue_posts(self, arg):
+        """Display posts from the modqueue"""
         text = ''
-        if notes is None:
-            return f"user {redditor_username} doesn't have any user notes"
-
-        for note in notes['ns']:
-            warning = warnings[note['w']]
-            when = datetime.datetime.fromtimestamp(note['t'])
-            note = note['n']
-            text += (f"<!date^{int(when.timestamp())}^{warning} at {{date_short}} {{time}}: {note}|"
-                     f"{warning} at {when.isoformat()}: {note}>\n")
-        return text
-    except prawcore.exceptions.NotFound:
-        return f"user {redditor_username} not found"
+        for s in self.sr.mod.modqueue(only='submissions'):
+            text += s.title + '\n' + s.url + '\n'
+        self._send_text(text)
 
 
-def do_modqueue_posts(sr):
-    """Display posts from the modqueue"""
-    text = ''
-    for s in sr.mod.modqueue(only='submissions'):
-        text += s.title + '\n' + s.url + '\n'
-    return text
+    def do_fortune(self, args):
+        """Like a Chinese fortune cookie, but less yummy"""
+        self._send_text(subprocess.check_output('/usr/games/fortune').decode())
 
 
-def do_fortune():
-    """Like a Chinese fortune cookie, but less yummy"""
-    return subprocess.check_output('/usr/games/fortune').decode()
-
-
-def do_add_domain_tag(sr, url_text, color):
-    """Add a tag to a domain"""
-    toolbox_data = json.loads(sr.wiki['toolbox'].content_md)
-    if re.match('<.*>', url_text):
-        url_text = url_text[1:-1]
-    url = urllib.parse.urlparse(url_text)
-    final_url = url.netloc
-    if len(url.path) > 1:
-        final_url += url.path
-    if not re.match(r'\#[0-9a-f]{6}', color, re.IGNORECASE):
-        return f"{color} is not a good color on you!"
-    entry = [tag for tag in toolbox_data['domainTags'] if tag['name'] == final_url]
-    if entry:
-        entry['color'] = color
-    else:
-        toolbox_data['domainTags'].append({'name': final_url, 'color': color})
-    sr.wiki['toolbox'].edit(json.dumps(toolbox_data), 'Updated by slackbot')
-    return f"Added color {color} for domain {final_url}"
+    def do_add_domain_tag(self, url_text, color):
+        """Add a tag to a domain"""
+        toolbox_data = json.loads(self.sr.wiki['toolbox'].content_md)
+        if re.match('<.*>', url_text):
+            url_text = url_text[1:-1]
+        url = urllib.parse.urlparse(url_text)
+        final_url = url.netloc
+        if len(url.path) > 1:
+            final_url += url.path
+        if not re.match(r'\#[0-9a-f]{6}', color, re.IGNORECASE):
+            self._send_text(f"{color} is not a good color on you!")
+            return
+        entry = [tag for tag in toolbox_data['domainTags'] if tag['name'] == final_url]
+        if entry:
+            entry['color'] = color
+        else:
+            toolbox_data['domainTags'].append({'name': final_url, 'color': color})
+        self.sr.wiki['toolbox'].edit(json.dumps(toolbox_data), 'Updated by slack')
+        self._send_text(f"Added color {color} for domain {final_url}")
 
 
 if __name__ == '__main__':
