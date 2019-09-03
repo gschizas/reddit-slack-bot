f053cbb507f9cf35c0b7e617984ba26b0f192e25
George Schizas <gschizas@gmail.com>
2019-09-03 22:51:50 +0300

    Add some formatting in error message

--- slack_bot.py
+++ slack_bot.py
@@ -324,7 +324,7 @@ class SlackbotShell(cmd.Cmd):
         if place == '':
             self._send_text(
                 ('You need to first set a default location\n'
-                f'Try {shell.trigger_words[0]} weather LOCATION'), is_error=True)
+                f'Try `{shell.trigger_words[0]} weather LOCATION`'), is_error=True)
         if place in ('brexit', 'pompeii'):
             title = 'the floor is lava'
             with open('img/lava.png', 'rb') as f:
