1a58a2509d4e33b9dd0db460bfd4c56d5d42530f
George Schizas <gschizas@gmail.com>
2020-10-30 22:04:18 +0200

    Handle slack links

--- command_shell.py
+++ command_shell.py
@@ -392,6 +392,8 @@ class SlackbotShell(cmd.Cmd):
     @staticmethod
     def _extract_real_thread_id(thread_id):
         if '/' in thread_id:
+            if thread_id.startswith('<') and thread_id.endswith('>'):  # slack link
+                thread_id = thread_id[1:-1]
             if thread_id.startswith('http://') or thread_id.startswith('https://'):
                 thread_id = thread_id.split('/')[6]
             elif thread_id.startswith('/'):
