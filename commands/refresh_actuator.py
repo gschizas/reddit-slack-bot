382953143e55c9ed42517d3bcb6378ca369e3024
George Schizas <gschizas@gmail.com>
2022-03-15 15:34:44 +0200

    Allow multiple deployments on one command

--- commands/refresh_actuator.py
+++ commands/refresh_actuator.py
@@ -75,9 +75,9 @@ def _user_allowed(slack_user_id, allowed_users):
 
 @actuator.command('refresh')
 @click.argument('namespace', type=OpenShiftNamespace())
-@click.argument('deployment', type=str)
+@click.argument('deployments', type=str, nargs=-1)
 @click.pass_context
-def refresh_actuator(ctx, namespace, deployment):
+def refresh_actuator(ctx, namespace, deployments):
     namespace_obj = _actuator_config()[namespace]
     server_url = namespace_obj['url']
     allowed_users = namespace_obj['users']
@@ -92,38 +92,39 @@ def refresh_actuator(ctx, namespace, deployment):
     ses = requests.session()
     openshift_token = namespace_obj['openshift_token']
     ses.headers['Authorization'] = 'Bearer ' + openshift_token
-    all_pods_raw = ses.get(
-        server_url + f"api/v1/namespaces/{namespace.lower()}/pods",
-        params={'labelSelector': f'deployment={deployment}'})
-    if not all_pods_raw.ok:
-        chat(ctx).send_file(file_data=all_pods_raw.content, filename='error.txt')
-        return
-    all_pods = all_pods_raw.json()
+    for deployment in deployments:
+        all_pods_raw = ses.get(
+            server_url + f"api/v1/namespaces/{namespace.lower()}/pods",
+            params={'labelSelector': f'deployment={deployment}'})
+        if not all_pods_raw.ok:
+            chat(ctx).send_file(file_data=all_pods_raw.content, filename='error.txt')
+            return
+        all_pods = all_pods_raw.json()
 
-    login_cmd = subprocess.run(['oc', 'login', f'--token={openshift_token}', f'--server={server_url}'], capture_output=True)
-    if login_cmd.returncode != 0:
-        chat(ctx).send_text("Error while logging in:\n```" + login_cmd.stderr.decode().strip() + "```", is_error=True)
-        return
-    pods_to_refresh = [pod['metadata']['name'] for pod in all_pods['items']]
-    if len(pods_to_refresh) == 0:
-        chat(ctx).send_text(f"Couldn't find any pods on {namespace} to refresh for {deployment}", is_error=True)
-        return
-    for pod_to_refresh in pods_to_refresh:
-        port_fwd = subprocess.Popen(['oc', 'port-forward', pod_to_refresh, '9999:8778'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        while True:
-            if port_fwd.poll() is not None:
-                if port_fwd.returncode != 0:
-                    port_fwd.stderr.flush()
-                    err_line = port_fwd.stderr.readline()
-                    logger(ctx).debug(err_line.decode().strip())
-                break
-            out_line = port_fwd.stdout.readline()
-            logger(ctx).debug(out_line.decode().strip())
-            if out_line == b'Forwarding from 127.0.0.1:9999 -> 8778\n':
-                logger(ctx).debug("Port forward Listening ok")
-                break
-            time.sleep(0.2)
-        refresh_result = requests.post("http://localhost:9999/actuator/refresh", proxies={'http': None, 'https': None})
-        # refresh_result = requests.get("http://localhost:9999/actuator/configprops", proxies={'http': None, 'https': None})
-        chat(ctx).send_file(file_data=refresh_result.content, filename=f'actuator-refresh-{pod_to_refresh}.json')
-        port_fwd.terminate()
+        login_cmd = subprocess.run(['oc', 'login', f'--token={openshift_token}', f'--server={server_url}'], capture_output=True)
+        if login_cmd.returncode != 0:
+            chat(ctx).send_text("Error while logging in:\n```" + login_cmd.stderr.decode().strip() + "```", is_error=True)
+            return
+        pods_to_refresh = [pod['metadata']['name'] for pod in all_pods['items']]
+        if len(pods_to_refresh) == 0:
+            chat(ctx).send_text(f"Couldn't find any pods on {namespace} to refresh for {deployment}", is_error=True)
+            return
+        for pod_to_refresh in pods_to_refresh:
+            port_fwd = subprocess.Popen(['oc', 'port-forward', pod_to_refresh, '9999:8778'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            while True:
+                if port_fwd.poll() is not None:
+                    if port_fwd.returncode != 0:
+                        port_fwd.stderr.flush()
+                        err_line = port_fwd.stderr.readline()
+                        logger(ctx).debug(err_line.decode().strip())
+                    break
+                out_line = port_fwd.stdout.readline()
+                logger(ctx).debug(out_line.decode().strip())
+                if out_line == b'Forwarding from 127.0.0.1:9999 -> 8778\n':
+                    logger(ctx).debug("Port forward Listening ok")
+                    break
+                time.sleep(0.2)
+            refresh_result = requests.post("http://localhost:9999/actuator/refresh", proxies={'http': None, 'https': None})
+            # refresh_result = requests.get("http://localhost:9999/actuator/configprops", proxies={'http': None, 'https': None})
+            chat(ctx).send_file(file_data=refresh_result.content, filename=f'actuator-refresh-{pod_to_refresh}.json')
+            port_fwd.terminate()
