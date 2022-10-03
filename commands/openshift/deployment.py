5905a260ecf15e9aa8513621e4eb306faae6aef3
George Schizas <gschizas@gmail.com>
2022-10-03 12:20:06 +0300

    Show deployment list as grid or as Excel file

--- commands/openshift/deployment.py
+++ commands/openshift/deployment.py
@@ -1,11 +1,15 @@
 import json
 
 import click
+import pandas as pd
+import io
+from tabulate import tabulate
 
 from commands import gyrobot, chat
 from commands.openshift.api import get_deployments, change_pause_state
 from commands.openshift.common import read_config, OpenShiftNamespace, check_security
 
+REMOVE_DEPLOYMENT_KEYS = ['Containers', 'Images', 'Selector']
 _deployment_config = read_config('OPENSHIFT_DEPLOYMENT')
 
 
@@ -19,11 +23,20 @@ def deployment(ctx: click.Context):
 
 @deployment.command('list')
 @click.argument('namespace', type=OpenShiftNamespace(_deployment_config))
+@click.option('-x', '--excel', is_flag=True, default=False)
 @click.pass_context
 @check_security
-def list_deployments(ctx, namespace):
+def list_deployments(ctx, namespace, excel: bool):
     deployments = get_deployments(ctx.obj['config'][namespace], namespace)
-    chat(ctx).send_file(json.dumps(deployments).encode(), filename='deployments.json')
+    if excel:
+        deployments_df = pd.DataFrame(deployments)
+        with io.BytesIO() as deployments_output:
+            deployments_df.reset_index(drop=True).to_excel(deployments_output)
+            chat(ctx).send_file(deployments_output.getvalue(), filename='deployments.xlsx')
+    else:
+        deployments = [{k: v for k, v in dep.items() if k not in REMOVE_DEPLOYMENT_KEYS} for dep in deployments]
+        deployments_markdown = tabulate(deployments, headers='keys', tablefmt='fancy_outline')
+        chat(ctx).send_file(deployments_markdown.encode(), filename='deployments.md')
 
 
 @deployment.command('pause')
