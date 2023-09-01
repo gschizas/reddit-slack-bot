e02358dbe126d9415db00ee198a19bc8270565f5
George Schizas <gschizas@gmail.com>
2023-09-01 16:00:42 +0300

    Clarify code

--- src/commands/openshift/refresh_actuator.py
+++ src/commands/openshift/refresh_actuator.py
@@ -113,7 +113,8 @@ def _get_pods(ctx, namespace, server_url, ses, deployment):
 
 def _send_results(ctx, pod_to_refresh, pod_env_before, refresh_result, pod_env_after, refresh_actuator_result):
     chat(ctx).send_file(pod_env_before.content, filename='pod_env_before.json')
-    if refresh_result.ok and refresh_actuator_result and all([type(rar) is str for rar in refresh_actuator_result.values()]):
+    value_types = [type(rar) is str for rar in refresh_actuator_result.values()]
+    if refresh_result.ok and refresh_actuator_result and all(value_types):
                     # refresh_actuator_result_list = sorted([rar for rar in refresh_actuator_result])
         refresh_actuator_result_list = rangify(refresh_actuator_result)
         chat(ctx).send_text('```\n' + '\n'.join(refresh_actuator_result_list) + '\n```\n')
