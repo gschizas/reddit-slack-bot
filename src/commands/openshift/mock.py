793189a326f9adef59252fd74d39414a5e6ed035
George Schizas <gschizas@gmail.com>
2023-09-04 19:36:08 +0300

    Use the general security check for mock as well

--- src/commands/openshift/mock.py
+++ src/commands/openshift/mock.py
@@ -1,6 +1,3 @@
-import json
-import os
-import pathlib
 import re
 import subprocess
 from string import Template
@@ -9,31 +6,21 @@ import click
 from ruamel.yaml import YAML
 
 from commands import gyrobot, chat, DefaultCommandGroup
-from commands.openshift.common import OpenShiftNamespace, azure_login
+from commands.openshift.common import OpenShiftNamespace, azure_login, check_security, read_config
 
 yaml = YAML()
 
 
 def _mock_config():
-    if os.environ['MOCK_CONFIGURATION'].startswith('/'):
-        config_file = pathlib.Path(os.environ['MOCK_CONFIGURATION'])
-    else:
-        config_file = pathlib.Path('config') / os.environ['MOCK_CONFIGURATION']
-    with config_file.open() as f:
-        mock_config = json.load(f)
-    with config_file.with_suffix('.credentials.yml').open() as f:
-        credentials = yaml.load(f)
-    for env in mock_config['environments']:
-        if env in credentials:
-            mock_config['environments'][env]['credentials'] = credentials[env]
-    default_environment = mock_config.get('default_environment', {})
-    for env_name, env in mock_config['environments'].items():
+    config = read_config('MOCK_CONFIGURATION')
+    default_environment = config.get('default_environment', {})
+    for env_name, env in config['environments'].items():
         if 'status' not in env:
             env['status'] = default_environment['status']
         for status, microservice_vars in env['status'].items():
             env['status'][status] = default_environment.get('status', {}).get(status, {}) | microservice_vars
         env['vartemplate'] = env.get('vartemplate', {}) | default_environment.get('vartemplate', {})
-    return mock_config
+    return config
 
 
 def _masked_oc_password(variable_list):
@@ -51,9 +38,9 @@ def _masked_oc_password(variable_list):
     return result
 
 
-def _get_project_name(mock_config, environment):
-    project_name = mock_config['environments'][environment].get('projectNameOverride', environment.lower())
-    project_prefix = mock_config['environments'][environment].get('projectPrefix')
+def _get_project_name(env_config, environment):
+    project_name = env_config.get('projectNameOverride', environment.lower())
+    project_prefix = env_config.get('projectPrefix')
     if project_prefix:
         project_name = project_prefix + '-' + project_name
     return project_name
@@ -64,7 +51,7 @@ def _get_project_name(mock_config, environment):
 def mock(ctx: click.Context):
     ctx.ensure_object(dict)
     ctx.obj['config'] = _mock_config()
-    ctx.obj['security_text'] = {'set': 'set mock status', 'check': 'check mock status'}
+    ctx.obj['security_text'] = {'set': 'switch mock status', 'check': 'view mock status'}
 
 
 @mock.command(default_command=True,
@@ -79,17 +66,15 @@ def mock_default(ctx: click.core.Context, environment: str, mock_status: OpenShi
 
 
 @mock.command('set')
-@click.argument('environment', type=OpenShiftNamespace(_mock_config()['environments'], force_upper=True))
+@click.argument('namespace', type=OpenShiftNamespace(_mock_config()['environments'], force_upper=True))
 @click.argument('mock_status')
 @click.pass_context
-def set_mock(ctx, environment: str, mock_status: str):
+@check_security
+def set_mock(ctx, namespace: str, mock_status: str):
     """Switch openshift mock status on environment"""
     mock_config = ctx.obj['config']
-    if chat(ctx).user_id not in mock_config['allowed_users']:
-        chat(ctx).send_text(f"You don't have permission to switch mock status.", is_error=True)
-        return
     env_vars = mock_config['env_vars']
-    env_config = mock_config['environments'][environment]
+    env_config = mock_config['environments'][namespace]
     valid_mock_statuses = [k.upper() for k in env_config['status'].keys()]
     mock_status = mock_status.upper()
     if mock_status not in valid_mock_statuses:
@@ -100,7 +85,7 @@ def set_mock(ctx, environment: str, mock_status: str):
     result_text = ""
     site = env_config['site']
     prefix = env_config['prefix']
-    project_name = _get_project_name(mock_config, environment)
+    project_name = _get_project_name(env_config, namespace)
 
     if site == 'azure':
         result_text += azure_login(
@@ -126,7 +111,7 @@ def set_mock(ctx, environment: str, mock_status: str):
     if _check_recursive(ctx, vartemplates):
         return
 
-    chat(ctx).send_text(f"Setting mock status to {mock_status} for project {project_name} on {environment}...")
+    chat(ctx).send_text(f"Setting mock status to {mock_status} for project {project_name} on {namespace}...")
 
     for microservice_info, status in statuses.items():
         microservice, env_variable_value = _get_environment_values(env_vars, vartemplates, microservice_info, status)
@@ -169,21 +154,20 @@ def _get_environment_values(env_vars, vartemplates, microservice_info, status):
 
 
 @mock.command('check')
-@click.argument('environment', type=OpenShiftNamespace(_mock_config()['environments'], force_upper=True))
+@click.argument('namespace', type=OpenShiftNamespace(_mock_config()['environments'], force_upper=True))
 @click.pass_context
-def mock_check(ctx, environment):
+@check_security
+def mock_check(ctx, namespace):
     """View current status of environment"""
-    mock_config = ctx.obj['config']
-    if chat(ctx).user_id not in mock_config['allowed_users']:
-        chat(ctx).send_text(f"You don't have permission to view mock status.", is_error=True)
-    oc_token = mock_config['environments'][environment]['credentials']
-    site = mock_config['environments'][environment]['site']
+    env_config = ctx.obj['config']['environments'][namespace]
+    oc_token = env_config['credentials']
+    site = env_config['site']
     result_text = subprocess.check_output(['oc', 'login', site, f'--token={oc_token}']).decode() + '\n' * 3
-    prefix = mock_config['environments'][environment]['prefix']
-    project_name = _get_project_name(mock_config, environment)
+    prefix = env_config['prefix']
+    project_name = _get_project_name(env_config, namespace)
     result_text += subprocess.check_output(['oc', 'project', project_name]).decode() + '\n' * 3
-    first_status = list(mock_config['environments'][environment]['status'].keys())[0]
-    microservices = list(mock_config['environments'][environment]['status'][first_status].keys())
+    first_status = list(env_config['status'].keys())[0]
+    microservices = list(env_config['status'][first_status].keys())
     for microservice in microservices:
         env_var_list = subprocess.check_output(['oc', 'env', prefix + microservice, '--list']).decode()
         env_var_list = _masked_oc_password(env_var_list)
