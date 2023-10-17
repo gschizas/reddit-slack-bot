b18f7dc06e1cb09a58d6c2b31ba2c0bee2cdf818
George Schizas <gschizas@gmail.com>
2023-10-17 15:07:03 +0300

    Refactor query to a constant

--- src/commands/reddit/database.py
+++ src/commands/reddit/database.py
@@ -6,14 +6,7 @@ from tabulate import tabulate
 
 from commands import gyrobot, chat
 
-
-@gyrobot.command('too_many_posts')
-@click.pass_context
-def too_many_posts(ctx):
-    """Show users with too many posts in the last 24 hours"""
-    with psycopg.connect(os.environ['GYROBOT_DATABASE_URL']) as conn:
-        with conn.cursor() as cur:
-            rows_raw = cur.execute("""\
+SQL_TOO_MANY_POSTS = """\
                 select
                     author,
                     count(*)
@@ -26,7 +19,16 @@ def too_many_posts(ctx):
                 having
                     count(*) > 2
                 order by
-                    count(*) desc;""")
+                    count(*) desc;"""
+
+
+@gyrobot.command('too_many_posts')
+@click.pass_context
+def too_many_posts(ctx):
+    """Show users with too many posts in the last 24 hours"""
+    with psycopg.connect(os.environ['GYROBOT_DATABASE_URL']) as conn:
+        with conn.cursor() as cur:
+            rows_raw = cur.execute(SQL_TOO_MANY_POSTS)
             rows = rows_raw.fetchall()
             headers = [col.name for col in rows_raw.description]
     result_table = [dict(zip(headers, row)) for row in rows]
