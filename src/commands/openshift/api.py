bc819c945d0bc29f390099305af64e4fdf6cf4e5
George Schizas <gschizas@gmail.com>
2024-02-07 18:10:59 +0200

    Remove type hinting as it's not compatible with Python 3.9

--- src/commands/openshift/api.py
+++ src/commands/openshift/api.py
@@ -34,8 +34,7 @@ def _get_table(ses, url):
     return result
 
 
-def _openshift_session(ctx: ExtendedContext, namespace: str, enforce_content_type: bool = True) \
-        -> tuple(str, requests.Session):
+def _openshift_session(ctx: ExtendedContext, namespace: str, enforce_content_type: bool = True):
     config: dict = env_config(ctx, namespace)
     openshift_token = config['credentials']
     server_url = config['url']
