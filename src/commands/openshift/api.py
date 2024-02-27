4126326a91105c53e39d99ae38e3c1cbf36917c0
George Schizas <gschizas@gmail.com>
2024-02-27 16:41:44 +0200

    Include apps api client

--- src/commands/openshift/api.py
+++ src/commands/openshift/api.py
@@ -135,8 +135,9 @@ class KubernetesConnection:
             kubernetes_configuration.ssl_ca_cert = self.cert_authority
 
         self.api_client = kubernetes.client.ApiClient(kubernetes_configuration)
-        self.core_v1_api = kubernetes.client.CoreV1Api(self.api_client)
+        self.apps_v1_api = kubernetes.client.AppsV1Api(self.api_client)
         self.batch_v1_api = kubernetes.client.BatchV1Api(self.api_client)
+        self.core_v1_api = kubernetes.client.CoreV1Api(self.api_client)
         self.well_known_api = kubernetes.client.WellKnownApi(self.api_client)
         return self
 
