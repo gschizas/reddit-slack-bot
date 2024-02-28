a716bbd68408cfbd1fe85d4bfc780e1eecad7e0b
George Schizas <gschizas@gmail.com>
2024-02-28 09:33:53 +0200

    Switch deployments to native API
    (not working at the moment)

--- src/commands/openshift/deployment.py
+++ src/commands/openshift/deployment.py
@@ -5,7 +5,7 @@ import click
 
 from commands import gyrobot
 from commands.extended_context import ExtendedContext
-from commands.openshift.api_obsolete_1 import get_deployments, change_deployment_pause_state
+from commands.openshift.api import KubernetesConnection
 from commands.openshift.common import read_config, OpenShiftNamespace, check_security
 
 REMOVE_DEPLOYMENT_KEYS = ['Containers', 'Images', 'Selector']
@@ -26,44 +26,55 @@ def deployment(ctx: ExtendedContext):
 @click.pass_context
 @check_security
 def list_deployments(ctx: ExtendedContext, namespace: str, excel: bool):
-    deployments = get_deployments(ctx, namespace)
+    with KubernetesConnection(ctx, namespace) as k8s:
+        deployments = k8s.apps_v1_api.list_namespaced_deployment(k8s.project_name)
     if not excel:
-        deployments = [{k: v for k, v in dep.items() if k not in REMOVE_DEPLOYMENT_KEYS} for dep in deployments]
-    ctx.chat.send_table(title='deployments', table=deployments, send_as_excel=excel)
+        deployment_table = [{k: v for k, v in dep.items() if k not in REMOVE_DEPLOYMENT_KEYS} for dep in deployments]
+    else:
+        deployment_table = deployments.items()
+    ctx.chat.send_table(title='deployments', table=deployment_table, send_as_excel=excel)
 
 
 @deployment.command('pause')
 @click.argument('namespace', type=OpenShiftNamespace(_deployment_config))
+@click.option('-x', '--excel', is_flag=True, default=False)
 @click.pass_context
 @check_security
-def pause_deployment(ctx: ExtendedContext, namespace: str):
-    deployments = get_deployments(ctx, namespace)
-    result = []
-    for one_deployment in deployments:
-        result.append(change_deployment_pause_state(ctx, namespace, one_deployment['Name'], True))
-    result_markdown = _make_deployments_table(result)
-    ctx.chat.send_table(title='deployments', table=result_markdown)
-    # ctx.chat.send_file(json.dumps(result).encode(), filename='deployments.json')
+def pause_deployment(ctx: ExtendedContext, namespace: str, excel: bool):
+    with KubernetesConnection(ctx, namespace) as k8s:
+        deployments = k8s.apps_v1_api.list_namespaced_deployment(k8s.project_name)
+        result = []
+        for one_deployment in deployments:
+            result.append(k8s.apps_v1_api.patch_namespaced_deployment(
+                one_deployment.metadata.name, k8s.project_name,
+                {'spec': {'paused': True}}))
+        result_markdown = _make_deployments_table(result)
+    ctx.chat.send_file(json.dumps(result).encode(), filename='deployments.json')
+    ctx.chat.send_table(title='deployments', table=result_markdown, send_as_excel=excel)
 
 
 @deployment.command('resume')
 @click.argument('namespace', type=OpenShiftNamespace(_deployment_config))
+@click.option('-x', '--excel', is_flag=True, default=False)
 @click.pass_context
 @check_security
-def resume_deployment(ctx: ExtendedContext, namespace):
-    deployments = get_deployments(ctx, namespace)
+def resume_deployment(ctx: ExtendedContext, namespace, excel: bool):
+    with KubernetesConnection(ctx, namespace) as k8s:
+        deployments = k8s.apps_v1_api.list_namespaced_deployment(k8s.project_name)
     result = []
     for one_deployment in deployments:
-        result.append(change_deployment_pause_state(ctx, namespace, one_deployment['Name'], None))
-    ctx.chat.send_table(title='deployments', table=_make_deployments_table(result))
-    # ctx.chat.send_file(json.dumps(result).encode(), filename='deployments.json')
+        result.append(k8s.apps_v1_api.patch_namespaced_deployment(one_deployment.metadata.name, k8s.project_name,
+                                                                  {'spec': {'paused': None}}))
+    ctx.chat.send_file(json.dumps(result).encode(), filename='deployments.json')
+    ctx.chat.send_table(title='deployments', table=_make_deployments_table(result), send_as_excel=excel)
+    ctx.chat.send_file(json.dumps(result).encode(), filename='deployments.json')
 
 
 def _make_deployments_table(result) -> List[Dict]:
     return [{
-        'Namespace': dep['metadata']['namespace'],
-        'Name': dep['metadata']['name'],
-        'Replicas': dep['status'].get('replicas'),
-        'Updated Replicas': dep['status'].get('updatedReplicas'),
-        'Ready Replicas': dep['status'].get('readyReplicas'),
-        'Available Replicas': dep['status'].get('availableReplicas')} for dep in result]
+        'Namespace': dep.metadata.namespace,
+        'Name': dep.metadata.name,
+        'Replicas': dep.status.replicas,
+        'Updated Replicas': dep.status.updated_replicas,
+        'Ready Replicas': dep.status.ready_replicas,
+        'Available Replicas': dep.status.available_replicas} for dep in result]
